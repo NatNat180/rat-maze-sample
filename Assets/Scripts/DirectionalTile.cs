a17fa8cc27726e79c63d962f3598ca72430acc5f
Argall, Nathan <Nathan.Argall@gmail.com>
2018-11-03 16:57:24 -0500

    initial commit - sample for beginning of rat maze

---
+++ Assets/Scripts/DirectionalTile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalTile : MonoBehaviour
+{
+
+    private const string UP = "UP";
+    private const string RIGHT = "RIGHT";
+    private const string DOWN = "DOWN";
+    private const string LEFT = "LEFT";
+    private string currentDirection;
+
+    public string CurrentDirection
+    {
+        get { return currentDirection; }
+    }
+
+    void Start()
+    {
+        currentDirection = UP;
+    }
+
+    void Update()
+    {
+		Debug.Log(currentDirection);
+    }
+
+    void OnMouseDown()
+    {
+        transform.Rotate(0, 90, 0);
+        switch (currentDirection)
+        {
+            case UP:
+                currentDirection = RIGHT;
+                break;
+            case RIGHT:
+                currentDirection = DOWN;
+                break;
+            case DOWN:
+                currentDirection = LEFT;
+                break;
+            case LEFT:
+                currentDirection = UP;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/RatBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatBehaviour : MonoBehaviour
+{
+
+    private Rigidbody ratBody;
+    private float speed = 3f;
+    private bool moveToggle = false;
+
+    void Start()
+    {
+        ratBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Get movement toggle from spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveToggle = !moveToggle;
+        }
+
+        // mouse movement based on movement toggle
+        if (moveToggle)
+        {
+            ratBody.velocity = transform.forward * speed;
+        }
+        else
+        {
+            ratBody.velocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "DirectionalTile")
+        {
+            DirectionalTile tile = collider.gameObject.GetComponent<DirectionalTile>();
+            switch (tile.CurrentDirection)
+            {
+                case "UP":
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case "RIGHT":
+                    transform.rotation = Quaternion.Euler(0, 90, 0);
+                    break;
+                case "DOWN":
+                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                    break;
+                case "LEFT":
+                    transform.rotation = Quaternion.Euler(0, 270, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
